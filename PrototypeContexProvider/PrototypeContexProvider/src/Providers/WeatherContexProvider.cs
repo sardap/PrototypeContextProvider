bddcc015e194978695d0d6cc950cb24345038387
sardap <pfsarda23@gmail.com>
2018-08-19 19:18:50 +1000

    Updated gitignore

---
+++ PrototypeContexProvider/PrototypeContexProvider/src/Providers/WeatherContexProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeContexProvider.src
+{
+	// api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3e0b77a7c48d0ef47d2b3ddfdc2d9bc3 
+	public class WeatherContexProvider
+	{
+
+	}
+}
